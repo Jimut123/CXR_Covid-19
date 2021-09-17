218eee781714007eb46f300f90a6bedf26108920
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-09-17 09:34:12 +0530

    50 epochs, 100 is overkill

--- Comparison/NASNetLarge.py
+++ Comparison/NASNetLarge.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 ###########################
-EPOCHS = 100
+EPOCHS = 50
 MODEL_NAME = 'NASNetLarge'
 IMG_SIZE = '331x331'
 OUTPUT_LAYERS = '128-32-3'
@@ -289,8 +289,8 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow import keras
 # batch_size = 100
 # valid_batch_size = 32
-batch_size = 32
-valid_batch_size = 32
+batch_size = 8
+valid_batch_size = 8
 train_gen = get_data_generator(df_train, train_idx, for_training=True, batch_size=batch_size)
 valid_gen = get_data_generator(df_val, test_idx, for_training=True, batch_size=valid_batch_size)
 
