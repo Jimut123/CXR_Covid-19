ba02b2c4c35ce60482ca4d7a00fdabdda74e1d91
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-09-18 10:37:17 +0530

    Update InceptionResNetV2.py

--- Comparison/InceptionResNetV2.py
+++ Comparison/InceptionResNetV2.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 ###########################
-EPOCHS = 1
+EPOCHS = 50
 MODEL_NAME = 'InceptionResNetV2'
 IMG_SIZE = '360x360'
 OUTPUT_LAYERS = '128-32-3'
