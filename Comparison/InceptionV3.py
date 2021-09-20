627b733c583a3a94b6ebd463f791bdda70d52e60
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-09-20 18:24:43 +0530

    Update InceptionV3.py

--- Comparison/InceptionV3.py
+++ Comparison/InceptionV3.py
@@ -289,8 +289,8 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow import keras
 # batch_size = 100
 # valid_batch_size = 32
-batch_size = 4
-valid_batch_size = 4
+batch_size = 32
+valid_batch_size = 32
 train_gen = get_data_generator(df_train, train_idx, for_training=True, batch_size=batch_size)
 valid_gen = get_data_generator(df_val, test_idx, for_training=True, batch_size=valid_batch_size)
 
