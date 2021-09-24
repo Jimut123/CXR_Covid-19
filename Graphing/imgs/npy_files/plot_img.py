b0d76e25c280b82c4ccfaeca24fca821a148dbf2
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-09-25 02:06:11 +0530

    added

--- Graphing/imgs/npy_files/plot_img.py
+++ Graphing/imgs/npy_files/plot_img.py
@@ -1,13 +1,5 @@
-import cv2
-import glob
-import numpy as np
-import matplotlib.pyplot as plt
-
-all_files = glob.glob("*.npy")
-#print(all_files)
-
-for file_name in all_files:
-    get_img_name = file_name.split('_')[0]+"_"+file_name.split('_')[1]
+"""
+ get_img_name = file_name.split('_')[0]+"_"+file_name.split('_')[1]
     print(get_img_name)
 
     get_img_name_folder = "img/"+get_img_name+".png"
@@ -45,6 +37,54 @@ for file_name in all_files:
     merged_img = cv2.cvtColor(merged_img, cv2.COLOR_BGR2GRAY)
     plt.imshow(img2,cmap='hot')
     plt.show()
+"""
+
+
+import cv2
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+all_files = glob.glob("*.npy")
+#print(all_files)
+
+
+for file_name in all_files:
+    get_img_name = file_name.split('_')[0]+"_"+file_name.split('_')[1]
+    print(get_img_name)
+
+    get_img_name_folder = "img/"+get_img_name+".png"
+    get_img = cv2.imread(get_img_name_folder)
+    get_img = cv2.cvtColor(get_img, cv2.COLOR_BGR2GRAY)
+    get_img = np.array(get_img)
+
+    heat_map = np.load(file_name)
+    heat_map = cv2.resize(heat_map, (get_img.shape[0],get_img.shape[1]))
+    cmap = plt.cm.hot
+    norm = plt.Normalize(vmin=heat_map.min(), vmax=heat_map.max())
+    heat_map = cmap(norm(heat_map))
+
+    
+    # img3 = np.array(img3*255.0,np.int32)
+
+    # plt.imshow(heat_map,cmap='hot')
+    # plt.show()
+
+    # plt.imshow(get_img,cmap='gray')
+    # plt.show()
+
+    plt.imsave(str(file_name.split('.')[0]+".png"),heat_map)
+
+    print("Img = Max = ",get_img.max()," Min = ",get_img.min())
+    print("heatmap = Max = ",heat_map.max()," Min = ",heat_map.min())
+    print(get_img.shape)
+    
+
+    
+    # merged_img = get_img*0.5 + heat_map*0.5
+    # merged_img = cv2.cvtColor(merged_img, cv2.COLOR_BGR2GRAY)
+    # plt.imshow(merged_img,cmap='hot')
+    # plt.show()
 
     #break
 
