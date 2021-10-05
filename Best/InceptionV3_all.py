787ddbe73bd507ae83fd39f735e2b92c0449d3e6
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-10-05 17:15:43 +0530

    Update InceptionV3_all.py

--- Best/InceptionV3_all.py
+++ Best/InceptionV3_all.py
@@ -3,8 +3,8 @@ import cv2
 import matplotlib.pyplot as plt
 
 ###########################
-EPOCHS = 1
-MODEL_NAME = 'InceptionV3'
+EPOCHS = 100
+MODEL_NAME = 'InceptionV3_ghapla'
 IMG_SIZE = '360x360'
 OUTPUT_LAYERS = '128-32-3'
 ###########################
