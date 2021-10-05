688de89f9069be0ee88868f35d67933d2898e0e9
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-10-05 09:05:51 +0530

    move ds ghapla

--- ablation/move_images_betw_ds.py
+++ ablation/move_images_betw_ds.py
@@ -6,6 +6,10 @@ import glob
 ['COVID-19_Radiography_Dataset/Normal', 'COVID-19_Radiography_Dataset/Lung_Opacity', 'COVID-19_Radiography_Dataset/Viral Pneumonia', 'COVID-19_Radiography_Dataset/README.md.txt', 'COVID-19_Radiography_Dataset/Viral Pneumonia.metadata.xlsx', 'COVID-19_Radiography_Dataset/COVID.metadata.xlsx', 'COVID-19_Radiography_Dataset/Normal.metadata.xlsx', 'COVID-19_Radiography_Dataset/Lung_Opacity.metadata.xlsx', 'COVID-19_Radiography_Dataset/COVID']
 """
 
+"""
+covid  normal  pneumonia
+"""
+
 all_new_ds_normal = glob.glob('COVID-19_Radiography_Dataset/Normal/*')
 print(len(all_new_ds_normal))
 print(all_new_ds_normal[:10])
@@ -18,6 +22,8 @@ print(all_new_ds_covid[:10])
 all_new_ds_vn = glob.glob('COVID-19_Radiography_Dataset/Viral Pneumonia/*')
 print(len(all_new_ds_vn))
 print(all_new_ds_vn[:10])
+
+
 
 
 
