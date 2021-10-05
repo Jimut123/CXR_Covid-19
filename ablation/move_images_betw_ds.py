0402595dab1d7c15891973b18519ce69abdc8b50
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2021-10-05 08:50:36 +0530

    modified sub

---
+++ ablation/move_images_betw_ds.py
@@ -0,0 +1,23 @@
+import glob
+
+# Use smote, data augmentation
+
+"""
+['COVID-19_Radiography_Dataset/Normal', 'COVID-19_Radiography_Dataset/Lung_Opacity', 'COVID-19_Radiography_Dataset/Viral Pneumonia', 'COVID-19_Radiography_Dataset/README.md.txt', 'COVID-19_Radiography_Dataset/Viral Pneumonia.metadata.xlsx', 'COVID-19_Radiography_Dataset/COVID.metadata.xlsx', 'COVID-19_Radiography_Dataset/Normal.metadata.xlsx', 'COVID-19_Radiography_Dataset/Lung_Opacity.metadata.xlsx', 'COVID-19_Radiography_Dataset/COVID']
+"""
+
+all_new_ds_normal = glob.glob('COVID-19_Radiography_Dataset/Normal/*')
+print(len(all_new_ds_normal))
+print(all_new_ds_normal[:10])
+
+all_new_ds_covid = glob.glob('COVID-19_Radiography_Dataset/COVID/*')
+print(len(all_new_ds_covid))
+print(all_new_ds_covid[:10])
+
+
+all_new_ds_vn = glob.glob('COVID-19_Radiography_Dataset/Viral Pneumonia/*')
+print(len(all_new_ds_vn))
+print(all_new_ds_vn[:10])
+
+
+
